2cfc07f075c66560eecbfbfa8030ca1c1aba4594
Aki Gao <xag9bb@virginia.edu>
2017-05-15 14:39:54 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# React Redux Template
+A basic template for a React & Redux application.
+
+#### To use
+Checkout this repo, install dependencies, then run the following commands in the terminal.
+
+```
+> git clone https://github.com/iknowhtml/react-redux-template.git
+> cd react-redux-template
+> yarn install
+> yarn start
+```
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015']
+      }
+    }]
+  },
+
+  resolveLoader: {
+    moduleExtensions: ['-loader']
+  },
+
+  devServer: {
+    contentBase: './',
+    port: 8080
+  }
+};
