e249fe442056bf1a85d63eda0fd42adaa367b43a
Aki Gao <xag9bb@virginia.edu>
2017-05-16 14:09:00 -0400

    refactor sparkline into reusable chart component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from '../containers/search_bar';
 import WeatherList from '../containers/weather_list';
+
 const App = () => (
   <div>
     <SearchBar />
